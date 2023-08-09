bc17c073e865305eb59e9df6ea1b7596d7009b0b
ahlixinjie <ahlixinjie@gmail.com>
2023-08-09 23:56:47 +0800

    log support metadata

--- log/log.go
+++ log/log.go
@@ -1,10 +1,19 @@
 package log
 
 import (
+	"context"
+	"github.com/ahlixinjie/go-utils/collections/list"
+	"github.com/ahlixinjie/mongoose/transport/common"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"google.golang.org/grpc/metadata"
 	"gopkg.in/natefinch/lumberjack.v2"
 	"os"
+	"strings"
+)
+
+const (
+	Region = "fiction-region"
 )
 
 var logger *zap.Logger
@@ -14,7 +23,7 @@ func init() {
 
 	tee := zapcore.NewTee(writeOnFileCore)
 
-	logger = zap.New(tee)
+	logger = zap.New(tee, zap.WithCaller(true))
 }
 
 func getEncoder() zapcore.Encoder {
@@ -44,4 +53,15 @@ func getLogWriteSyncer(filePath string) zapcore.WriteSyncer {
 
 func GetLogger() *zap.Logger {
 	return logger
+}
+
+func GetLoggerWithCtx(ctx context.Context) *zap.Logger {
+	var l = logger
+	if region := list.GetFirstElement(metadata.ValueFromIncomingContext(ctx, Region)); len(region) != 0 {
+		l = l.With(zap.String(Region, region))
+	}
+	if requestID := list.GetFirstElement(metadata.ValueFromIncomingContext(ctx, strings.ToLower(common.HeaderRequestID))); len(requestID) != 0 {
+		l = l.With(zap.String(common.HeaderRequestID, requestID))
+	}
+	return l
 }
--- log_test.go
+++ log_test.go
@@ -1,13 +1,22 @@
 package mongoose
 
 import (
+	"context"
 	"errors"
 	"github.com/ahlixinjie/mongoose/log"
+	"github.com/ahlixinjie/mongoose/transport/common"
 	"go.uber.org/zap"
+	"google.golang.org/grpc/metadata"
 	"testing"
 )
 
 func TestLog(t *testing.T) {
 	log.GetLogger().With(zap.String("hello", "world")).Info("can I")
 	log.GetLogger().With(zap.Error(errors.New("custom error"))).Error("msg")
+
+	ctx := metadata.NewIncomingContext(context.Background(), metadata.New(map[string]string{
+		common.HeaderRequestID: "123",
+		log.Region:             "vn",
+	}))
+	log.GetLoggerWithCtx(ctx).Info("haha")
 }
